4f1ad12ef0fcef70fb7d6957a500a4f29b1b0d12
Juan Matus <juanm@nicasource.com>
2022-02-19 17:51:50 -0600

    added fix for framer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <Component {...pageProps} />
-  );
+function MyApp({ Component, pageProps, router }: AppProps) {
+  // these lines fix a recent issue with framer and nextjs
+  const [isLoaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return <></>;
+  }
+
+  // router.route returns the route you're on, e.g. '/' or '/[id]'
+
+  return <Component {...pageProps} key={router.route} />;
 }
 
 export default MyApp;
